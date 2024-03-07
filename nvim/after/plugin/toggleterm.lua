2c77e1f65f1b8b3016804cdf4eacf8d67f7addc4
Nishant Racherla <nishant.racherla@gmail.com>
2024-03-06 22:13:24 -0800

    feat: init commit

---
+++ nvim/after/plugin/toggleterm.lua
@@ -0,0 +1,24 @@
+local status, toggleterm = pcall(require, "toggleterm")
+if not status then return end
+
+toggleterm.setup({
+  size = 20,
+  open_mapping = [[<C-\>]],
+  hide_numbers = true,
+  shade_filetypes = {},
+  shading_factor = 2,
+  start_in_insert = true,
+  insert_mappings = true,
+  persist_size = true,
+  direction = "float",
+  close_on_exit = true,
+  shell = vim.o.shell,
+  float_opts = {
+    border = "single",
+    winblend = 0,
+    highlights = {
+      border = "Normal",
+      background = "Normal",
+    },
+  },
+})
---
+++ nvim/lua/t1lt/init.lua
@@ -0,0 +1,2 @@
+require("t1lt.remap")
+require("t1lt.set")
